2b936ebb93f9bd9bd95b3184a9c66b125b8f6679
outmane bzioui <bziouioutmane@gmail.com>
2025-01-02 13:18:43 +0100

    add docker files

--- api/index.js
+++ api/index.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 app.use(express.json({ limit: "5mb" }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   })
 );
@@ -49,7 +49,8 @@ app.get("*", (req, res) => {
 //global error handler
 app.use(globalerrorHandler);
 
-server.listen(8000, () => {
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => {
   console.log("Server started on port 8000");
 });
 
